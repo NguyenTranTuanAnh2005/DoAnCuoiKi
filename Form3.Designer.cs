2c56615e8db9c562e527400325e4678e92b5ad1c
DELL <DELL@BONDEVERLOPER>
2025-12-18 20:29:35 +0700

    Add project files.

---
+++ Form3.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace QuanLyBanHang
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.btnOut = new System.Windows.Forms.Button();
+            this.btnKKSL = new System.Windows.Forms.Button();
+            this.btnQLK = new System.Windows.Forms.Button();
+            this.btnAddItean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOut
+            // 
+            this.btnOut.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnOut.BackgroundImage = global::QuanLyBanHang.Properties.Resources.c7dc6656fb6b112a043d0c74b5326084;
+            this.btnOut.Image = global::QuanLyBanHang.Properties.Resources.istockphoto_878320722_2048x2048;
+            this.btnOut.Location = new System.Drawing.Point(700, 300);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(300, 70);
+            this.btnOut.TabIndex = 0;
+            this.btnOut.Text = "Thoát";
+            this.btnOut.UseVisualStyleBackColor = true;
+            // 
+            // btnKKSL
+            // 
+            this.btnKKSL.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnKKSL.Image = global::QuanLyBanHang.Properties.Resources.istockphoto_878320722_2048x2048;
+            this.btnKKSL.Location = new System.Drawing.Point(255, 300);
+            this.btnKKSL.Name = "btnKKSL";
+            this.btnKKSL.Size = new System.Drawing.Size(300, 70);
+            this.btnKKSL.TabIndex = 0;
+            this.btnKKSL.Text = "Kiểm kê hàng hóa";
+            this.btnKKSL.UseVisualStyleBackColor = true;
+            // 
+            // btnQLK
+            // 
+            this.btnQLK.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnQLK.Image = global::QuanLyBanHang.Properties.Resources.istockphoto_878320722_2048x2048;
+            this.btnQLK.Location = new System.Drawing.Point(700, 155);
+            this.btnQLK.Name = "btnQLK";
+            this.btnQLK.Size = new System.Drawing.Size(300, 70);
+            this.btnQLK.TabIndex = 0;
+            this.btnQLK.Text = "Quản lý kho";
+            this.btnQLK.UseVisualStyleBackColor = true;
+            // 
+            // btnAddItean
+            // 
+            this.btnAddItean.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAddItean.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAddItean.Image = global::QuanLyBanHang.Properties.Resources.istockphoto_878320722_2048x2048;
+            this.btnAddItean.Location = new System.Drawing.Point(255, 155);
+            this.btnAddItean.Name = "btnAddItean";
+            this.btnAddItean.Size = new System.Drawing.Size(300, 70);
+            this.btnAddItean.TabIndex = 0;
+            this.btnAddItean.Text = "Nhập hàng";
+            this.btnAddItean.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.btnKKSL);
+            this.Controls.Add(this.btnQLK);
+            this.Controls.Add(this.btnAddItean);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form3";
+            this.Text = "Quản lý cửa hàng";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddItean;
+        private System.Windows.Forms.Button btnQLK;
+        private System.Windows.Forms.Button btnKKSL;
+        private System.Windows.Forms.Button btnOut;
+    }
+}
---
+++ Form6.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace QuanLyBanHang
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SoLuong3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameIteam3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IdIteam3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnoutt = new System.Windows.Forms.Button();
+            this.SoLuonDaBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chi tiết số lượng hàng";
+            // 
+            // SoLuong3
+            // 
+            this.SoLuong3.HeaderText = "Số Lượng còn lại";
+            this.SoLuong3.MinimumWidth = 6;
+            this.SoLuong3.Name = "SoLuong3";
+            this.SoLuong3.Width = 125;
+            // 
+            // NameIteam3
+            // 
+            this.NameIteam3.HeaderText = "Tên Hàng";
+            this.NameIteam3.MinimumWidth = 6;
+            this.NameIteam3.Name = "NameIteam3";
+            this.NameIteam3.Width = 125;
+            // 
+            // IdIteam3
+            // 
+            this.IdIteam3.HeaderText = "Mã món hàng";
+            this.IdIteam3.MinimumWidth = 6;
+            this.IdIteam3.Name = "IdIteam3";
+            this.IdIteam3.Width = 125;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdIteam3,
+            this.NameIteam3,
+            this.SoLuong3,
+            this.SoLuonDaBan});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(629, 437);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnoutt
+            // 
+            this.btnoutt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnoutt.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnoutt.Location = new System.Drawing.Point(491, 601);
+            this.btnoutt.Name = "btnoutt";
+            this.btnoutt.Size = new System.Drawing.Size(75, 23);
+            this.btnoutt.TabIndex = 2;
+            this.btnoutt.Text = "Thoát";
+            this.btnoutt.UseVisualStyleBackColor = true;
+            // 
+            // SoLuonDaBan
+            // 
+            this.SoLuonDaBan.HeaderText = "Số lượng hàng đã bán";
+            this.SoLuonDaBan.MinimumWidth = 6;
+            this.SoLuonDaBan.Name = "SoLuonDaBan";
+            this.SoLuonDaBan.Width = 125;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QuanLyBanHang.Properties.Resources.images;
+            this.ClientSize = new System.Drawing.Size(653, 668);
+            this.Controls.Add(this.btnoutt);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form6";
+            this.Text = "chi tiết số lượng hàng";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoLuong3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameIteam3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdIteam3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnoutt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoLuonDaBan;
+    }
+}
